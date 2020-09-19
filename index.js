5077e1d878c366387ad2aa6380b46a367fa8b270
Phạm Quang Huy <39458058+huybopbi@users.noreply.github.com>
2020-09-19 15:41:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cmd = require('node-cmd');
 app.get("/", (request, response) => response.sendFile(__dirname + "/view/index.html"));
 const listener = app.listen(process.env.PORT, () => logger("Đã mở tại port: " + listener.address().port), 0);
 
-if (process.env.REFRESHING == 'on') setTimeout(() => {
+ setTimeout(() => {
 	console.log("Đang làm mới sau 10 phút!");
 	cmd.run("pm2 restart 0");
 }, 600000);
